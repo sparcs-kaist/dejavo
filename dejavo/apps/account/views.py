e090bd7dea6da8f0136e38e1f10b759d30d84f52
Horknox <horknox@gmail.com>
2015-01-13 23:02:52 +0900

    Create view-functions which were not created but exist in dejavo/urls.py

--- dejavo/apps/account/views.py
+++ dejavo/apps/account/views.py
@@ -3,6 +3,13 @@ from django.shortcuts import render
 
 import sys
 
+def login(request):
+    return HttpResponse(__name__ + '.' + sys._getframe().f_code.co_name) 
+
+def logout(request):
+    return HttpResponse(__name__ + '.' + sys._getframe().f_code.co_name) 
+
+
 def main(request):
     return HttpResponse(__name__ + '.' + sys._getframe().f_code.co_name) 
 
--- dejavo/apps/zabo/views.py
+++ dejavo/apps/zabo/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 
 import sys
 
+def main(request):
+    return HttpResponse(__name__ + '.' + sys._getframe().f_code.co_name)
+
 def create(request):
     return HttpResponse(__name__ + '.' + sys._getframe().f_code.co_name)
 
