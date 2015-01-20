b18c6c9a39526e09c8698f9bb5dd9970ea4d4732
coffee <poapoap@gmail.com>
2015-01-21 02:06:35 +0900

    Add more manage pages.(claim, log, block)

--- dejavo/apps/manage/views.py
+++ dejavo/apps/manage/views.py
@@ -4,20 +4,19 @@ from django.shortcuts import render
 import sys
 
 def main(request):
-    return render(request, 'manage/main.html', {'content':__name__ + '.' + sys._getframe().f_code.co_name})
-    #return HttpResponse(__name__ + '.' + sys._getframe().f_code.co_name)
+    return render(request, 'manage/main.html', {})
 
 def list_claim(request):
-    return HttpResponse(__name__ + '.' + sys._getframe().f_code.co_name)
+    return render(request, 'manage/claim.html', {})
 
 def view_claim(request):
     return HttpResponse(__name__ + '.' + sys._getframe().f_code.co_name)
 
 def list_log(request):
-    return HttpResponse(__name__ + '.' + sys._getframe().f_code.co_name)
+    return render(request, 'manage/log.html', {})
 
 def list_block(request):
-    return HttpResponse(__name__ + '.' + sys._getframe().f_code.co_name)
+    return render(request, 'manage/block.html', {})
 
 def set_article_block(request):
     return HttpResponse(__name__ + '.' + sys._getframe().f_code.co_name)
