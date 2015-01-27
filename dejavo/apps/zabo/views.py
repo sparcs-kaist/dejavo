620dd85a6404c43e72c4ff5104a18a97d80df88a
coffee <poapoap@gmail.com>
2015-01-27 15:47:00 +0900

    Implemented the functions for timeslots, questions and answers

--- dejavo/apps/zabo/views.py
+++ dejavo/apps/zabo/views.py
@@ -5,16 +5,19 @@ from django.core.exceptions import ValidationError
 from django.contrib.auth import get_user_model
 
 from accept_checker.decorators import require_accept_formats
-from dejavo.apps.zabo.models import Article, Question, Answer
+from dejavo.apps.zabo.models import Article, Timeslot, Question, Answer
 
 import sys
+
 
 def main(request):
     return HttpResponse(__name__ + '.' + sys._getframe().f_code.co_name)
 
+
 @require_accept_formats(['text/html', 'application/json'])
 @require_http_methods(['POST'])
 def create(request):
+
     if not request.user.is_authenticated():
         return JsonResponse(
                 status = 401,
@@ -41,6 +44,7 @@ def create(request):
         response = HttpResponse(status = 201, content = '')
         response['Location'] = '/article/' + str(new_article.id) + '/edit/'
         return response
+
 
 @require_accept_formats(['text/html', 'application/json'])
 @require_http_methods(['GET'])
@@ -127,14 +131,107 @@ def edit_article(request, article_id):
                     },
                 )
 
+
+@require_accept_formats(['application/json'])
+@require_http_methods(['POST'])
 def create_timeslot(request, article_id):
-    return HttpResponse(__name__ + '.' + sys._getframe().f_code.co_name)
 
+    if not request.user.is_authenticated():
+        return JsonResponse(
+                status = 401,
+                data = {
+                    'error' : 'User does not authorized'
+                    },
+                )
+
+    try:
+        article = Article.objects.get(id = article_id)
+        if request.user not in article.owner.all():
+            msg = 'User does not own the article'
+            return JsonResponse(status = 403, data = { 'error' : msg })
+        timeslot = Timeslot(article = article,
+                timeslot_type = request.POST.get('type', ''),
+                start_time = request.POST.get('start_time', ''),
+                end_time = request.POST.get('end_time', ''),
+                label = request.POST.get('label', '')
+                )
+        timeslot.full_clean()
+        timeslot.save()
+
+        return JsonResponse(
+                status = 200,
+                data = timeslot.as_json()
+                )
+
+    except Article.DoesNotExist:
+        msg = 'Article(' + str(article_id) + ') does not exist'
+        return JsonResponse(status = 404, data = { 'error' : msg })
+
+    except ValidationError as e:
+        return JsonResponse(
+                status = 400,
+                data = {
+                    'error' : 'Invaild format',
+                    'msg' : e.message_dict,
+                    },
+                )
+
+
+@require_accept_formats(['application/json'])
+@require_http_methods(['POST'])
 def delete_timeslot(request, article_id, timeslot_id):
-    return HttpResponse(__name__ + '.' + sys._getframe().f_code.co_name)
 
+    if not request.user.is_authenticated():
+        return JsonResponse(
+                status = 401,
+                data = {
+                    'error' : 'User does not authorized'
+                    },
+                )
+
+    try:
+        timeslot = Timeslot.objects.get(id = timeslot_id)
+        if request.user not in timeslot.article.owner.all():
+            return JsonResponse(
+                    status = 401,
+                    data = {
+                        'error' : 'User does not have the permission'
+                        },
+                    )
+
+        timeslot.delete()
+
+        return JsonResponse(status = 200, data = {})
+
+    except Timeslot.DoesNotExist:
+        msg = 'Timeslot(' + str(timeslot_id) + ') does not exist'
+        return JsonResponse(status = 404, data = { 'error' : msg })
+
+
+@require_accept_formats(['text/html', 'application/json'])
+@require_http_methods(['GET'])
 def view_qna(request, article_id):
-    return HttpResponse(__name__ + '.' + sys._getframe().f_code.co_name)
+
+    try:
+        article = Article.objects.get(id = article_id)
+        qna = []
+        for q in Question.objects.filter(article__id = article_id):
+            qna.append(q.as_json())
+        
+        # TODO render page showing only questions and answers
+        if request.ACCEPT_FORMAT == 'html':
+            #return render(request, 'zabo/qna.html', {})
+            return HttpResponse()
+
+        return JsonResponse(status = 200, data = { 'qna' : qna })
+
+    except Article.DoesNotExist:
+        msg = 'Article(' + str(article_id) + ') does not exist'
+        if request.ACCEPT_FORMAT == 'html':
+            return HttpResponse(status = 404, content = msg)
+        else:
+            return JsonResponse(status = 404, data = { 'error' : msg })
+
 
 @require_accept_formats(['application/json'])
 @require_http_methods(['POST', 'PUT'])
@@ -183,21 +280,59 @@ def create_question(request, article_id):
 @require_accept_formats(['application/json'])
 @require_http_methods(['GET'])
 def load_question(request, article_id):
+
     try:
         question_list = []
         article = Article.objects.get(id = article_id)
         for q in Question.objects.filter(article__id = article_id):
             question_list.append(q.as_json())
-        return JsonResponse(status=200, data={'questions':question_list})
+
+        return JsonResponse(status=200, data={ 'questions' : question_list })
+
     except Article.DoesNotExist:
         return JsonResponse(
-                status=404,
-                data={'error':'Not Found: article_id : ' + article_id}
+                status = 404,
+                data = {
+                    'error' : 'article(' + article_id + ') does not exist'
+                    },
                 )
 
 
+@require_accept_formats(['application/json'])
+@require_http_methods(['POST'])
 def delete_question(request, article_id, question_id):
-    return HttpResponse(__name__ + '.' + sys._getframe().f_code.co_name)
+
+    if not request.user.is_authenticated():
+        return JsonResponse(
+                status = 401,
+                data = {
+                    'error' : 'User does not authorized'
+                    },
+                )
+    
+    try:
+        article = Article.objects.get(id = article_id)
+        question = Question.objects.get(id = question_id)
+        question.is_deleted = True
+        question.save()
+
+        return JsonResponse(status = 200, data = {})
+
+    except Article.DoesNotExist:
+        return JsonResponse(
+                status = 404,
+                data = {
+                    'error' : 'article(' + article_id + ') does not exist'
+                    },
+                )
+
+    except Question.DoesNotExist:
+        return JsonResponse(
+                status = 404,
+                data = {
+                    'error' : 'question(' + question_id + ') does not exist'
+                    },
+                )
 
 
 @require_accept_formats(['application/json'])
@@ -251,8 +386,52 @@ def create_answer(request, article_id, question_id):
                     },
                 )
 
+
+@require_accept_formats(['application/json'])
+@require_http_methods(['POST'])
 def delete_answer(request, article_id, question_id, answer_id):
-    return HttpResponse(__name__ + '.' + sys._getframe().f_code.co_name)
+
+    if not request.user.is_authenticated():
+        return JsonResponse(
+                status = 401,
+                data = {
+                    'error' : 'User does not authorized'
+                    },
+                )
+    
+    try:
+        article = Article.objects.get(id = article_id)
+        question = Question.objects.get(id = question_id)
+        answer = Answer.objects.get(id = answer_id)
+        answer.is_deleted = True
+        answer.save()
+
+        return JsonResponse(status = 200, data = {})
+
+    except Article.DoesNotExist:
+        return JsonResponse(
+                status = 404,
+                data = {
+                    'error' : 'article(' + article_id + ') does not exist'
+                    },
+                )
+
+    except Question.DoesNotExist:
+        return JsonResponse(
+                status = 404,
+                data = {
+                    'error' : 'question(' + question_id + ') does not exist'
+                    },
+                )
+
+    except Answer.DoesNotExist:
+        return JsonResponse(
+                status = 404,
+                data = {
+                    'error' : 'answer(' + answer_id + ') does not exist'
+                    },
+                )
+
 
 def edit_announcement(request, article_id, announcement_id):
     return HttpResponse(__name__ + '.' + sys._getframe().f_code.co_name)
