967b8251dc6c66f9476912223b6a7103236799bc
coffee <poapoap@gmail.com>
2015-01-27 15:46:13 +0900

    Change urls.py little bit for "/"

--- dejavo/urls.py
+++ dejavo/urls.py
@@ -9,9 +9,9 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'dejavo.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^$', 'dejavo.apps.zabo.views.main'),
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^/$', 'dejavo.apps.zabo.views.main'),
     url(r'^login/$', 'dejavo.apps.account.views.login'),
     url(r'^logout/$', 'dejavo.apps.account.views.logout'),
 
